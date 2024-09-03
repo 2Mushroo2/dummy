c783c0c6b17957303017c26fcfe5563dcd8c2bb6
roqval <valledor.roque@gmail.com>
2024-09-03 20:23:47 +0200

    New printed messages.

--- main.c
+++ main.c
@@ -7,12 +7,15 @@ int main() {
     int res = 0;
 
     //Hello world printing
-    printf("Hello World!\n");
+    printf("Hello World! This is a dummy repo\n");
 
     //Sum saved in variable res
     res = 5 + 3;
     //Print result at the console
     printf("The result is: %d\n", res);
+
+    //Function
+    justonefunction(void);
 
     return 0;
 }
