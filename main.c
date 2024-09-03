ca642f373e8e2cfa13ca9f68c518c4fa6288f3e6
roqval <valledor.roque@gmail.com>
2024-09-03 19:14:56 +0200

    First commit.

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+    //Init
+    int res = 0;
+
+    //Hello world printing
+    printf("Hello World!\n");
+
+    //Sum saved in variable res
+    res = 5 + 3;
+    //Print result at the console
+    printf("The result is: %d\n", res);
+
+    return 0;
+}
